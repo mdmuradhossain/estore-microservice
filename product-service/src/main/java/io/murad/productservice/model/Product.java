e64d96cfe829d5c4b3f2047a582c9a6044e295e1
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-01 21:03:50 +0600

    ProductRepository added

--- product-service/src/main/java/io/murad/productservice/model/Product.java
+++ product-service/src/main/java/io/murad/productservice/model/Product.java
@@ -1,19 +1,20 @@
 package io.murad.productservice.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
 @Builder
 public class Product {
     @Id
@@ -24,4 +25,17 @@ public class Product {
     private String description;
     private Double price;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Product product = (Product) o;
+
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 2042274511;
+    }
 }
---
+++ product-service/src/main/java/io/murad/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package io.murad.productservice.repository;
+
+import io.murad.productservice.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}
