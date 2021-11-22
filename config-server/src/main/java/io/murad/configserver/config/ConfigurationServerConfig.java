8bdaaaa99571562f0303e45a8fa2f340a69b9a6b
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-22 22:18:59 +0600

    ConfigServer Enabled

---
+++ config-server/src/main/java/io/murad/configserver/config/ConfigurationServerConfig.java
@@ -0,0 +1,9 @@
+package io.murad.configserver.config;
+
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigServer
+public class ConfigurationServerConfig {
+}
