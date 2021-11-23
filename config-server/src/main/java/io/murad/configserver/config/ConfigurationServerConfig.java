8b9a0c8c837f32316d6f799dbde41890154104f7
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-11-23 23:41:38 +0600

    properties file updated

--- config-server/src/main/java/io/murad/configserver/config/ConfigurationServerConfig.java
+++ config-server/src/main/java/io/murad/configserver/config/ConfigurationServerConfig.java
@@ -3,7 +3,7 @@ package io.murad.configserver.config;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableConfigServer
+//@Configuration
+//@EnableConfigServer
 public class ConfigurationServerConfig {
 }
